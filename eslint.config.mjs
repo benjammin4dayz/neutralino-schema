9eb60bf10d41337dc56877cf07fd66ba604481e8
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-10-25 13:39:07 -0400

    Update scripts; add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+];
